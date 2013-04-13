c1a26d5f6964893dd4d7b199378140ebfd77fcfe
Gary <fishermanpl@gmail.com>
2013-04-13 22:48:40 +0200

    Add windows extractor binary maps, vmaps and mmaps.

---
+++ contrib/windows_extractor_binary/make_mmaps.bat
@@ -0,0 +1,21 @@
+@echo off
+cls
+echo.
+echo                    Welcome to the Move Map Gen
+echo.
+echo You need 2GB of free space in disk, CTRL+C to stop process
+echo Hit Enter to start . . .
+pause>nul
+cls
+echo.
+echo.
+echo.
+md mmaps
+MoveMapGen.exe --silent --offMeshInput offmesh.txt
+cls
+echo.
+echo.
+echo.
+echo Process done! copy mmaps folder to the Emulator main directory
+echo Press any key to exit . . .
+pause>nul
